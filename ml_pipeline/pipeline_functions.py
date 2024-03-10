996068b161ea60815c11a16e2f40fe8555932328
Mohammad <mohammadimaan484@gmail.com>
2024-03-09 19:12:31 -0500

    added mp4 to mp3 convertor

--- ml_pipeline/pipeline_functions.py
+++ ml_pipeline/pipeline_functions.py
@@ -1,6 +1,6 @@
 from openai import OpenAI
 from tika import parser
-
+from moviepy.editor import *
 
 def speech_to_text(file_path):
     client = OpenAI()
@@ -9,8 +9,7 @@ def speech_to_text(file_path):
         model="whisper-1",  
         file=audio_file
     )
-    print(transcription.text)
-
+    return transcription.text
 
 
 def extract_text_from_file(file_path):
@@ -18,4 +17,23 @@ def extract_text_from_file(file_path):
     raw_text = parser.from_file(file_path) 
 
     # Print the extracted text 
-    print(raw_text['content']) 
+    return raw_text['content']
+
+def mp4_to_mp3(file_path):
+    # Specify the filename of the resulting MP3 file
+    mp3_file_path = f'#{file_path[:-4]}.mp3'
+
+    # Load the video file
+    video_clip = VideoFileClip(file_path)
+
+    # Extract the audio from the video clip
+    audio_clip = video_clip.audio
+
+    # Write the audio data to the MP3 file
+    audio_clip.write_audiofile(mp3_file_path)
+
+    # Close the clips
+    audio_clip.close()
+    video_clip.close()
+
+    return mp3_file_path
